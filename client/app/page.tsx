fcd6f9745baf7cc9a07d359061f395d23b9be794
minhduc <vuminhduc231003@gmail.com>
2025-07-03 19:20:21 +0700

    fix rerender error

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState, useRef } from "react";
 import ServicesSection from "@/components/ServicesSection";
 import AboutSection from "@/components/AboutSection";
 import { useModal } from "@/context/ModalContext";
+import Brand from "@/components/Brand";
 
 interface Project {
   id: number;
@@ -46,18 +47,6 @@ const mockProjects: Project[] = [
   },
 ];
 export default function Home() {
-  const logos = [
-    { name: "DJI", path: "/next.svg" },
-    { name: "H&M", path: "/next.svg" },
-    { name: "Zara", path: "/next.svg" },
-    { name: "Ferrari", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-    { name: "Reebok", path: "/next.svg" },
-  ];
   const [scroll, setScroll] = useState<number>(0);
   const [scrollYProgress, setScrollYProgress] = useState<any>(0);
   const projectRef = useRef<HTMLDivElement>(null);
@@ -137,52 +126,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="border-t border-gray-200">
-          <div className="container mx-auto px-4 py-10">
-            <div className="flex flex-col lg:flex-row items-center justify-between gap-10 ">
-              <div>
-                <p className="text-gray-500 text-lg w-[200px]">
-                  Trusted by{" "}
-                  <span className="font-bold text-gray-700 text-3xl">many</span>
-                </p>
-              </div>
-              <div className="overflow-hidden">
-                <div className="relative overflow-hidden w-full">
-                  <div className="flex overflow-hidden whitespace-nowrap">
-                    <motion.div
-                      className="flex"
-                      animate={{
-                        x: ["0%", "-50%"],
-                      }}
-                      transition={{
-                        duration: 10,
-                        ease: "linear",
-                        repeat: Infinity,
-                      }}
-                    >
-                      <div className="flex">
-                        {[...Array(6)].map(() =>
-                          logos.map((logo, index) => (
-                            <Image
-                              key={`first-${
-                                logo.name
-                              }-${index}-${Math.random()}`}
-                              src={logo.path}
-                              alt={`${logo.name} logo`}
-                              width={100}
-                              height={28}
-                              className="mx-8 object-contain filter grayscale opacity-60 hover:grayscale-0 hover:opacity-100"
-                            />
-                          ))
-                        )}
-                      </div>
-                    </motion.div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Brand />
         <section className="py-24 border-t border-gray-200" ref={projectRef}>
           <div className="container mx-auto px-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
---
+++ client/components/Brand.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { memo } from "react";
+
+const Brand = () => {
+  const logos = [
+    { name: "DJI", path: "/next.svg" },
+    { name: "H&M", path: "/next.svg" },
+    { name: "Zara", path: "/next.svg" },
+    { name: "Ferrari", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+    { name: "Reebok", path: "/next.svg" },
+  ];
+  return (
+    <section className="border-t border-gray-200">
+      <div className="container mx-auto px-4 py-10">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-10 ">
+          <div>
+            <p className="text-gray-500 text-lg w-[200px]">
+              Trusted by{" "}
+              <span className="font-bold text-gray-700 text-3xl">many</span>
+            </p>
+          </div>
+          <div className="overflow-hidden">
+            <div className="relative overflow-hidden w-full">
+              <div className="flex overflow-hidden whitespace-nowrap">
+                <motion.div
+                  className="flex"
+                  animate={{
+                    x: ["0%", "-50%"],
+                  }}
+                  transition={{
+                    duration: 10,
+                    ease: "linear",
+                    repeat: Infinity,
+                  }}
+                >
+                  <div className="flex">
+                    {[...Array(6)].map(() =>
+                      logos.map((logo, index) => (
+                        <Image
+                          key={`first-${logo.name}-${index}-${Math.random()}`}
+                          src={logo.path}
+                          alt={`${logo.name} logo`}
+                          width={100}
+                          height={28}
+                          className="mx-8 object-contain filter grayscale opacity-60 hover:grayscale-0 hover:opacity-100"
+                        />
+                      ))
+                    )}
+                  </div>
+                </motion.div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default memo(Brand);
